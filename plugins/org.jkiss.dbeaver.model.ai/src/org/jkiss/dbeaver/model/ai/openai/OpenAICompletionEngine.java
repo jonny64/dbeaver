7ac904cc15990d2637fa952ea8ae72b47943055a
jonny64 <astro64m@gmail.com>
2025-04-23 20:51:04 +0300

    +openai.local

--- plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/openai/OpenAICompletionEngine.java
+++ plugins/org.jkiss.dbeaver.model.ai/src/org/jkiss/dbeaver/model/ai/openai/OpenAICompletionEngine.java
@@ -165,7 +165,7 @@ public class OpenAICompletionEngine implements DAICompletionEngine {
 
     protected OpenAIClient createClient() throws DBException {
         return new OpenAIClient(
-            "https://api.openai.com/v1/",
+            "http://openai.local/v1/",
             List.of(new OpenAIRequestFilter(OpenAISettings.INSTANCE.token()))
         );
     }
